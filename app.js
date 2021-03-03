3e492ff3263c217d94f63dbc52b0c2f64abda9e6
Kornienko Aleksey <kornienko199004@gmail.com>
2021-03-03 23:07:17 +0300

    fix: fix backend bug
    fix: fix backend bug
    
    fix: change port
    
    fix: fix backend bug
    
    fix: fix backend bug

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const config = require('config');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const path = require('path');
 
 const app = express();
 
@@ -10,14 +11,24 @@ const PORT = config.get('port') || 5000;
 app.use(bodyParser.json());
 app.use('/api/top', require('./routes/top.routes'));
 
+// app.use(express.static(path.join(__dirname, 'build')));
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname+'/client/build/index.html'));
+});
+
 async function start() {
   try {
     await mongoose.connect(config.get('mongoUri'), {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-      useCreateIndex: true,
+      useCreateIndex: true
     });
-    app.listen(PORT, () => console.log(`App has been started on port ${PORT}...`));
+    app.listen(PORT, () =>
+      console.log(`App has been started on port ${PORT}...`)
+    );
   } catch (e) {
     console.log('Server Error', e.message);
     process.exit(1);
